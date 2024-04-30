eac8964a0871552bf691ff0215707def8d81d2e5
songyi <songyee112@naver.com>
2024-04-30 17:22:53 +0900

    로컬 스토리지 기능 구현중 (아직 실행 안됨)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoBoard from './component/TodoBoard';
 import './App.css' 
 
 function App() {
   const [inputvalue,setInputvalue] = useState('');
   const [todoList, setTodoList] = useState([]);
+
+  // 맵이 로드될 때 로컬 스토리지에서 할 일 목록을 가져와 설정
+
+  useEffect(()=> {
+    const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
+    if (savedTodoList) {
+      setTodoList(savedTodoList);
+    }
+  }, []);
+
+  // 할 일 목록이 변경될 때마다 로컬 스토리지에 저장 
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+
+
+
   const addItem = () => {
     setTodoList([...todoList,inputvalue])
   }
